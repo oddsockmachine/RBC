ecc9b7b9802a65aac462abb11d3ca3dc7f518544
David Walker <david.walker@anaplan.com>
2017-07-23 16:23:40 -0700

    installing mqtt broker on rpi

--- esp8266/examples/publish.py
+++ esp8266/examples/publish.py
@@ -1,30 +1,30 @@
-https://home-assistant.io/blog/2016/08/31/esp8266-and-micropython-part2/
-
-
-https://www.hackster.io/bucknalla/mqtt-micropython-044e77
+# https://home-assistant.io/blog/2016/08/31/esp8266-and-micropython-part2/
+# https://www.hackster.io/bucknalla/mqtt-micropython-044e77
 
 import ubinascii
 import machine
 from umqtt2 import MQTTClient
+import dht
+from time import sleep
+
+
 
 CONFIG = {
     "broker": "192.168.0.12",
     "sensor_pin": 0,
-    "client_id": b"esp8266_" + ubinascii.hexlify(machine.unique_id()),
-    "topic": b"topic",
+    "client_id": "esp8266_" + str(ubinascii.hexlify(machine.unique_id())),
+    "topic": "topic",
 }
 
 client = MQTTClient(CONFIG['client_id'], CONFIG['broker'])
-client.connect()
 
-data = "foo"
-client.publish('{}/{}'.format(CONFIG['topic'], CONFIG['client_id']), bytes(str(data), 'utf-8'))
-client.publish('topic', bytes(str(data), 'utf-8'))
+def connect():
+    ## TODO Auto-connect, retry-backoff, and catch errors
+    client.connect()
 
+def disconnect():
+    client.disconnect()
 
-
-import dht
-d = dht.DHT11(machine.Pin(5))
 
 def measure(_d, readings, log):
     h = []
@@ -39,14 +39,15 @@ def measure(_d, readings, log):
         print("{}c, {}%".format(temp,hum))
     return temp, hum
 
-measure(d, 3, True)
 
-from time import sleep
 def send_measurements(delay, log):
+    d = dht.DHT11(machine.Pin(5))
     vcc = machine.ADC(1)
+    ## TODO Add some try-catch to detect broken connection
     while True:
         t, h = measure(d, 3, log)
         batt = vcc.read()
         data = "{}c, {}%, {}vcc".format(t,h,batt)
         client.publish('{}/{}'.format(CONFIG['topic'], CONFIG['client_id']), bytes(str(data), 'utf-8'))
         sleep(delay)
+    return
