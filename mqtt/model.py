fed1fe15df03ca9e64b1cf12d06cfbbbe4e004ae
David Walker <david.walker@anaplan.com>
2017-10-28 09:15:53 -0700

    adding location graph to model

--- mqtt/model.py
+++ mqtt/model.py
@@ -2,20 +2,38 @@ import redis
 import rom
 from datetime import datetime
 from conf import load_config
+from uuid import uuid4
 config = load_config()
-
 rom.util.set_connection_settings(host=config['REDIS_URL'], port=config['REDIS_PORT'])
 
 
+class Location(rom.Model):
+    name = rom.String(required=True, index=True, keygen=rom.FULL_TEXT)
+    url = rom.String(required=True, index=True, unique=True, keygen=rom.FULL_TEXT)
+    description = rom.String(required=True)
+    parent = rom.ManyToOne('Location', on_delete='cascade')
+    children = rom.OneToMany('Location', column='parent')
+    # outline of grid coordinates for map
+
+class Sensor(rom.Model):
+    name = rom.String(required=True, index=True, keygen=rom.FULL_TEXT)
+    uid = rom.String(required=True, index=True, unique=True, keygen=rom.FULL_TEXT)
+    # uid = "~".join([_self.name, sensor_type, job_name, pin])  # Tag so we can identify this function later.
+    sensor_type = rom.String(required=True, index=True, keygen=rom.FULL_TEXT)  # What kind of sensor is this? eg DHT_11
+    pin = rom.String()  # Pin identifier on chip, adc, i2c
+    period = rom.Integer()  # In seconds
+    units = rom.String()  # Human readable units
+    node = rom.ManyToOne('Node', on_delete='cascade', required=True)
 
 class Node(rom.Model):
     name = rom.String(required=True, index=True, keygen=rom.FULL_TEXT)
     uid = rom.String(required=True, index=True, unique=True, keygen=rom.FULL_TEXT)
     presence = rom.Boolean(default=False)  # Is it working, or has LastWill fired
     location = rom.String(required=True)  # human readable location
+    location_url = rom.String(required=True)  # TODO tree-based nested url eg farm/field1/greenhouse3/bed2
     tags = rom.String(index=True, keygen=rom.FULL_TEXT)
-    jobs = rom.Json()
-    sensors = rom.Json()
+    # jobs = rom.Json()
+    sensors = rom.OneToMany('Sensor')
     created_at = rom.DateTime()  # When was this node first activated
     woken_at = rom.DateTime()  # When did this node last wake up
     power = rom.Float()  # Battery power remaining
@@ -23,7 +41,6 @@ class Node(rom.Model):
     lat = rom.Float()
     def geo_cb(data):
         return data
-
     geo_index = [
         # callback function passed to GeoIndex as the 2nd argument *must*
         # return a dictionary containing 'lon' and 'lat' values, as degrees
@@ -31,13 +48,15 @@ class Node(rom.Model):
         ]
 
     def init(self):
-        self.jobs = {}
-        self.sensors = {}
+        # self.jobs = {}
+        # self.sensors = {}
         self.created_at = datetime.now()
         self.woken_at = datetime.now()
+        self.presence = False
+        self.power = 0.0
         return self
 
-    def update_with_report(self, report):
+    def update_from_report(self, report):
         return
 
     def update_presence(self, presence):
@@ -47,6 +66,24 @@ class Node(rom.Model):
             self.presence = False
         self.save()
 
+    def to_json(self):
+        return self.__dict__.get('_data')
+
+def exists(uid):
+    n = Node.query.filter(uid=uid).all()
+    return (len(n) > 0)
+
+def create_node(name, location, location_url, lat, lon, tags):
+    uid = str(uuid4()).split("-")[0]
+    n = Node(name=name, uid=uid, lat=lat, lon=lon, location=location, location_url=location_url, tags=tags).init()
+    n.save()
+    print(n)
+    return n
+
+def create_node_from_report(report):
+    n = Node()
+    # n.save()
+    return n
 
 def get_node_by_name(name):
     n = Node.query.filter(name=name).all()
@@ -54,26 +91,74 @@ def get_node_by_name(name):
 
 def get_node_by_id(uid):
     n = Node.query.filter(uid=uid).all()
-    return n[0]
+    return n[0].to_json()
 
-# n1 = Node(name='foo', uid='123', lat=1.1, lon=0.9, location='place').init().save()
-# n2 = Node(name='bar', uid='456', lat=0.9, lon=1.1, location='place').init().save()
+def get_all_nodes():
+    n = Node.query.filter().all()
+    return map(lambda x: x.to_json(), n)
+
+def get_root_location():
+    locs = Location.query.filter(name='root').all()
+    if len(locs) == 1:
+        print("Root location already established")
+        return locs[0]
+    elif len(locs) == 0:
+        print("Creating new root location")
+        root = Location(name='root', url='root', description='root node')
+        root.save()
+        return root
+    else:
+        raise("multiple root locations found!")
+
+def create_location(name, description, parent):
+    url = parent.url.decode('utf-8') + '/' + name.replace(' ', '_').lower()
+    print(name, parent.name, parent.url, url)
+    new_loc = Location(name=name, url=url, description='Our apartment', parent=parent)
+    new_loc.save()
+    return new_loc
+
+def get_location_by_url(_url):
+    locs = Location.query.filter(url=_url).all()
+    # print(locs)
+    if len(locs)==1:
+        return locs[0]
+    elif len(locs)==0:
+        print("Location not found at url {}".format(url))
+        return None
+    else:
+        print("{} conflicting locations found at {}".format(len(locs), url))
+        return None
+
+def get_location_graph_under(location):
+    def recurse(location, graph):
+        for child in location.children:
+            graph['children'].append({'location': location.name, 'children': []})
+            recurse(child, graph)
+        return graph
+    graph = {'location': location.name, 'children': []}
+    graph = recurse(location, graph)
+    return graph
+
+
 # Node(name='bap', uid='265', lat=0.93, lon=1.15, location='place').init().save()
-# Node(name='baz', uid='364', lat=0.95, lon=1.11, location='place').init().save()
-# Node(name='fuz', uid='342', lat=0.91, lon=1.13, location='place').init().save()
-# Node(name='bip', uid='234', lat=0.92, lon=1.19, location='place').init().save()
 
 # print(get_node_by_id('123').name)
-# print(get_node_by_id('123').jobs)
-# print(get_node_by_id('123').presence)
 # print(get_node_by_name('bar').name)
-# print(get_node_by_name('bar').jobs)
-# print(get_node_by_name('bar').presence)
 #
+# points = Node.query \
+#     .near('geo_index', 1.0, 1.0, 2500.0, 'mi', 2) \
+#     .limit(0, 50) \
+#     .all()
+# for p in points:
+#     print(p.name, p.lat, p.lon)
 
-points = Node.query \
-    .near('geo_index', 1.0, 1.0, 2500.0, 'mi', 2) \
-    .limit(0, 50) \
-    .all()
-for p in points:
-    print(p.name, p.lat, p.lon)
+root_location = get_root_location()
+# bayside = create_location('3 Bayside', 'Our apartment', root_location)
+# balcony = create_location('Balcony', 'On the balcony', bayside)
+# living_room = create_location('Living Room', 'In the living room', bayside)
+print(root_location.children[0].children[0].name)
+print(root_location.children[0].children[0].url)
+
+print(get_location_by_url('root').children[0].name)
+print(get_location_by_url('root/3_bayside/balcony').children)
+# print(get_location_graph_under(root_location))
