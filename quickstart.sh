dfd520013c2389cea9c4ebc824fd16b922eb9288
David Walker <david.walker@anaplan.com>
2017-12-28 15:28:20 -0800

    add quickstart script to provision remote nodes

---
+++ quickstart.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/oddsockmachine/RBC.git
+
+sudo apt-get update
+sudo apt install tree
+python3 -V
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+# sudo dpkg-reconfigure locales
+cd RBC/nodule
+pyvenv venv
+source venv/bin/activate
+which python
+which pip
+python --version
+pip install -r requirements/requirements.txt
+# python nodule.py
