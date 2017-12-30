3aaad35625430d8b7cfb91930cc312972e97904b
David Walker <oddsockmachine@gmail.com>
2017-12-29 22:16:33 -0800

    hook api to ui

--- frontend/ui/src/app/api.clj
+++ frontend/ui/src/app/api.clj
@@ -6,14 +6,16 @@
 
 
 
-
+(defn recget
+  [keys data]
+  (reduce #(get %1 %2) data keys))
 
 (def graphql-url "http://dmip:5050/graphql")
 
 (defn graphql
   [query]
   (let [response (client/post graphql-url {:body query :content-type :json})
-        data (get (get (json/read-str (:body response)) "data") "query")]
+        data (recget ["data" "query"] (json/read-str (:body response)))]
     data))
   ; (:body (client/post graphql-url {:body query :content-type :json})))
 
@@ -21,9 +23,9 @@
   ; (let [nodules {"abc123" "balcony" "def456" "living room" "ghi789" "kitchen" "jkl012" "bedroom"}])
   (let [nodules ["abc123" "def456" "ghi789" "jkl012"]
         query "{ \"query\": \"{query{allNodules{nodes{name, uid}}}}\"}"
-        data (graphql query)]
+        data (recget ["allNodules" "nodes"] (graphql query))]
   ; (let [nodules [{:uid "abc123" :name "balcony"} {:uid "def456" :name "living room"} {:uid "ghi789" :name "kitchen"} {:uid "jkl012" :name "bedroom"}]]
     ; (prn (client/post "http://dmip:5050/graphql" {:data query}))
     (prn data)
     (prn nodules)
-    nodules))
+    data))
